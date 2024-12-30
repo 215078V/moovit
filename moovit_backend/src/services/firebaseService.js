c2e89ff3ae919e835bc7ecc28c2f9617445c5c04
Sandani Muthunayaka <muthunayakahst.21@uom.lk>
2024-12-30 21:09:00 +0530

    bugs fixed

--- moovit_backend/src/services/firebaseService.js
+++ moovit_backend/src/services/firebaseService.js
@@ -1,10 +1,11 @@
 const admin = require('firebase-admin');
+require('dotenv').config();
 
 const serviceAccount = require('../../serviceAccountKey.json');
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-    databaseURL: 'https://moovit-be-default-rtdb.firebaseio.com/',
+    databaseURL: process.env.DATABASE,
 });
 
 const db = admin.database();
